ce3c2e35b9ad99872f9d9e75af3fca17158c89c3
Jay Desai <jayd@sapphiregroup.com>
2017-05-27 13:05:05 +0200

    password validation bug fixes

--- 5.4/public/js/password-validation.js
+++ 5.4/public/js/password-validation.js
@@ -23,25 +23,12 @@ jQuery(document).ready(function($){
         $('#password_confirmation').password('toggle');
     });
 
-    //ensure password_confirmation matches password
 
-    //BASIC check to see if passwords match
-
-    //$('#password, #password_confirmation').on('keyup', function () {
-    //  if ($('#password').val() == '' && $('#password_confirmation').val() == '') {
-    //    $('#message').html('');
-    //  } else
-    //  if ($('#password').val() == $('#password_confirmation').val()) {
-    //    $('#message').html('Passwords Match!').css('color', 'green');
-    //  } else 
-    //    $('#message').html('Passwords Do Not Match!').css('color', 'red');
-    //});
-
-    //More thorough check to ensure passwords match as well as the strength of the password
+    //Password Strength and check to ensure passwords match
     $(document).ready(function() {
         var password1       = $('#password'); //id of first password field
         var password2       = $('#password_confirmation'); //id of second password field
-        var passwordsInfo   = $('#pass-info'); //id of indicator element
+        var passwordsInfo   = $('#result'); //id of indicator element
         
         passwordStrengthCheck(password1,password2,passwordsInfo); //call password check function
         
@@ -50,46 +37,82 @@ jQuery(document).ready(function($){
     function passwordStrengthCheck(password1, password2, passwordsInfo)
     {
         //Must contain 6 characters or more
-        var WeakPass = /(?=.{6,}).*/; 
-        //Must contain lower case letters and at least one digit.
-        var MediumPass = /^(?=\S*?[a-z])(?=\S*?[0-9])\S{6,}$/; 
-        //Must contain at least one upper case letter, one lower case letter and one digit.
-        var StrongPass = /^(?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])\S{6,}$/; 
-        //Must contain at least one upper case letter, one lower case letter and one digit.
-        var VryStrongPass = /^(?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?=\S*?[^\w\*])\S{6,}$/; 
+        var WeakPass = /(?=.{6,}).*/;
+        //Must contain 6 characters of lower case letters and at least one digit.
+        var MediumPass = /^(?=\S*?[a-z])(?=\S*?[0-9])\S{6,}$/;
+        //Must contain 6 characters of at least one upper case letter, one lower case letter and one digit.
+        var StrongPass = /^(?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])\S{6,}$/;
+        //Must contain 6 characters of at least one upper case letter, one lower case letter, one digit and one non-alphanumeric character.
+        var VryStrongPass = /^(?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?=\S*?[^\w\*])\S{6,}$/;
         
         $(password1).on('keyup', function(e) {
-            if(VryStrongPass.test(password1.val()))
+            if(password1.val() == '')
             {
-                passwordsInfo.removeClass().addClass('vrystrongpass').html("Very Strong! (Please don't forget your password now!)").css({'color':'green', 'font-size':'13px'});
+                passwordsInfo.removeClass().addClass('weakpass').html("Please specify a password.").css({'color':'red', 'font-size':'13px'});
+                if (password2.val !== '') {
+                    password2.val('');
+                }
+            }
+            else if(VryStrongPass.test(password1.val()))
+            {
+                passwordsInfo.removeClass().addClass('vrystrongpass').html("Very Strong Password!").css({'color':'green', 'font-size':'13px'});
+                if (password2.val !== '') {
+                    password2.val('');
+                }                
             }   
             else if(StrongPass.test(password1.val()))
             {
-                passwordsInfo.removeClass().addClass('strongpass').html("Strong! (Enter special chars to make it even stronger)").css({'color':'green', 'font-size':'13px'});
+                passwordsInfo.removeClass().addClass('strongpass').html("Strong Password!").css({'color':'green', 'font-size':'13px'});
+                if (password2.val !== '') {
+                    password2.val('');
+                }                
             }   
             else if(MediumPass.test(password1.val()))
             {
-                passwordsInfo.removeClass().addClass('goodpass').html("Good! (Enter an uppercase letter to make it stronger)").css({'color':'green', 'font-size':'13px'});
+                passwordsInfo.removeClass().addClass('goodpass').html("Good Password!").css({'color':'green', 'font-size':'13px'});
+                if (password2.val !== '') {
+                    password2.val('');
+                }                
             }
             else if(WeakPass.test(password1.val()))
             {
-                passwordsInfo.removeClass().addClass('stillweakpass').html("Still Weak! (Enter digits to make a good password)").css({'color':'green', 'font-size':'13px'});
+                passwordsInfo.removeClass().addClass('stillweakpass').html("Weak Password! (Enter digits to make it stronger)").css({'color':'green', 'font-size':'13px'});
+                if (password2.val !== '') {
+                    password2.val('');
+                }
             }
             else
             {
-                passwordsInfo.removeClass().addClass('weakpass').html("Very Weak! (Must be 6 or more chars)").css({'color':'red', 'font-size':'13px'});
+                passwordsInfo.removeClass().addClass('weakpass').html("Password must be 6 or more characters").css({'color':'red', 'font-size':'13px'});
             }
         });
         
         $(password2).on('keyup', function(e) {
-            
+
             if(password1.val() !== password2.val())
             {
-                passwordsInfo.removeClass().addClass('weakpass').html("Passwords do not match!").css({'color':'red', 'font-size':'13px'});
-            }else{
-                passwordsInfo.removeClass().addClass('goodpass').html("Passwords match!").css({'color':'green', 'font-size':'13px'});
+                if (password1.val().length < 6)
+                //if(WeakPass.test(password1.val()))
+                {
+                    passwordsInfo.removeClass().addClass('weakpass').html("Password must be 6 or more characters").css({'color':'red', 'font-size':'13px'});
+                }
+                else
+                {
+                    passwordsInfo.removeClass().addClass('weakpass').html("Passwords do not match!").css({'color':'red', 'font-size':'13px'});
+                }
             }
-                
+            else
+            {
+                if (password1.val().length < 6)
+                //if(WeakPass.test(password1.val()))
+                {
+                    passwordsInfo.removeClass().addClass('weakpass').html("Password must be 6 or more characters").css({'color':'red', 'font-size':'13px'});
+                }
+                else
+                {
+                    passwordsInfo.removeClass().addClass('weakpass').html("Passwords match!").css({'color':'green', 'font-size':'13px'});
+                }
+            }
         });
     }    
 
